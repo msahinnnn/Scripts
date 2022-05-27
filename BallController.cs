8a4d320e398c5da23533cd0da6149e4fc8ae4ac6
msahinnnn <mehmetsahin.99@outlook.com>
2022-05-28 01:44:09 +0300

    ball controller modification

--- BallController.cs
+++ BallController.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     public int myScore ;
+    
+
+    public void Update(){
+        GameObject.Find("MyTeam").GetComponent<TextMesh>().text = myScore + "";
+
+    }
+
     private void OnTriggerEnter(Collider other) {
         
         if(other.gameObject.tag == "Goal"){
             Debug.Log("GOOOOLLLL");
             transform.position = GameObject.Find("BallPosition").transform.position;
             this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            
+            GameObject.Find("Player").transform.position = GameObject.Find("PlayerPosition").transform.position;
 
             myScore += 1;
         }
+
+        if(other.gameObject.tag == "TouchLine"){
+            Debug.Log("OUT!");
+
+            transform.position = GameObject.Find("BallPosition").transform.position;
+            this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            
+            GameObject.Find("Player").transform.position = GameObject.Find("PlayerPosition").transform.position;
+        }
     }
 
-    public void Update(){
-        GameObject.Find("MyTeam").GetComponent<TextMesh>().text = myScore + "";
 
-    }
 
 
 }
