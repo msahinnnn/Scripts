1d1fa924e2ba4d167dea700786946a7b8e4bbcf8
msahinnnn <mehmetsahin.99@outlook.com>
2022-05-28 21:33:44 +0300

    BallController

--- BallController.cs
+++ BallController.cs
@@ -8,29 +8,23 @@ public class BallController : MonoBehaviour
     
 
     public void Update(){
-        GameObject.Find("MyTeam").GetComponent<TextMesh>().text = myScore + "";
+        
 
     }
 
     private void OnTriggerEnter(Collider other) {
+        transform.position = GameObject.Find("BallPosition").transform.position;
+        this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            
+        GameObject.Find("Player").transform.position = GameObject.Find("PlayerPosition").transform.position;
         
         if(other.gameObject.tag == "Goal"){
             Debug.Log("GOOOOLLLL");
-            transform.position = GameObject.Find("BallPosition").transform.position;
-            this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            
-            GameObject.Find("Player").transform.position = GameObject.Find("PlayerPosition").transform.position;
-
-            myScore += 1;
+            myScore += 1;        
         }
 
         if(other.gameObject.tag == "TouchLine"){
             Debug.Log("OUT!");
-
-            transform.position = GameObject.Find("BallPosition").transform.position;
-            this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            
-            GameObject.Find("Player").transform.position = GameObject.Find("PlayerPosition").transform.position;
         }
     }
 
